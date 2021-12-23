0baee2676d0324293d6ad18c1f4198176286f9c9
Your Name <you@example.com>
2021-12-23 16:25:13 +0100

    capire come fare unno si e uno no

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Editable from '../src/components/editable'
+import NotEditable from "./components/notEditable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      infoDef:{name:'',email:'',number:'',school:'',title:'',date:'',company:'',position:'',period:''},
+      infoChang:{},
+      number:0,
+    }
+
+    this.change = this.change.bind(this)
+    this.confirm = this.confirm.bind(this)
+    this.confirmNumber = this.confirmNumber.bind(this)
+    this.changeNumber = this.changeNumber.bind(this)
+  }
+
+
+  change(e) {
+    let name = e.target.id
+    let newObj = {}
+    newObj[name] = e.target.value
+    this.setState({
+      infoChang:{...this.state.infoChang,...newObj}
+    })
+    console.log(this.state)
+  }
+
+  confirm(e) {
+    e.preventDefault();
+    this.setState({
+      infoDef:{...this.state.infoDef,...this.state.infoChang},
+      infoChang:{}
+    })
+
+    console.log(this.state)
+  }
+
+  confirmNumber() {
+    this.setState({
+      number:1
+    })
+
+    console.log(this.state,'hello')
+  }
+
+  changeNumber() {
+    this.setState({
+      number:0
+    })
+
+    console.log(this.state,'ciao')
+  }
+
+
+
+
+
+  render() {
+    return(
+      <div>
+        {this.state.number===0?<Editable change={this.change} confirm={this.confirm} info={this.state.infoDef} changeNumber={this.confirmNumber}/>:<NotEditable info={this.state.infoDef} changeNumber={this.cNumber}/>}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/notEditable.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class NotEditable extends Component {
+
+    render() {
+        return(
+            <div>
+                <h3>{this.props.info.name}</h3>
+                <h3>{this.props.info.email}</h3>
+                <h3>{this.props.info.number}</h3>
+                <h3>{this.props.info.school}</h3>
+                <h3>{this.props.info.title}</h3>
+                <h3>{this.props.info.date}</h3>
+                <h3>{this.props.info.company}</h3>
+                <h3>{this.props.info.position}</h3>
+                <h3>{this.props.info.period}</h3>
+                <button onClick={this.props.changeNumber}>Change</button>
+            </div>
+        )
+    }
+}
+
+export default NotEditable 
